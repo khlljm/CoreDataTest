f5dda7dd044d502a015cc7d94d8c9767047551bc
khl <khl@khldeMacBook-Pro.local>
2017-02-23 13:36:34 +0800

    添加数据库管理类
    添加数据库管理类

---
+++ CoreDataTest/CoreDataManager.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.m
+//  CoreDataTest
+//  数据库管理类
+//  Created by khl on 2017/2/23.
+//  Copyright © 2017年 khl. All rights reserved.
+//
+
+#import "CoreDataManager.h"
+
+@implementation CoreDataManager
+
+@end
